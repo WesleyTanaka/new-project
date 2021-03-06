b17b9fc49f2ed95cdc63b52bc8f7413cf7a7f989
WesleyTanaka1 <wesleytanaka17@gmail.com>
2021-03-06 15:28:39 +0000

    Teste

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Oi!");
-    System.out. println ("Como vai?")
+    System.out. println("Como vai?");
+    System.out.println("OK, beleza!");
   }
 }
